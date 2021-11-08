c8e369f7ae8e4d2a561a524e6127bd57c2dd6528
Boggy <g.bogovac@outlook.com>
2021-11-08 19:04:34 +0100

    added node setup and gitignore

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+const numberOfBarrels = 30;
+const numberOfDays = 1000;
+const numberOfSlaves = 8;
+
+start(numberOfBarrels, numberOfDays, numberOfSlaves);
+
+function start(numberOfBarrels, numberOfDays, numberOfSlaves) {
+    
+};
